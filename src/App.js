34ac7b6fc81dd8a51595f6391f85fcb7513c3f00
Sayli555 <96167212+Sayli555@users.noreply.github.com>
2022-04-08 21:41:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const os=["Android","Blackberry","iPhone","Windows Phone"]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="title">REACT MOBILE OS</h1>
+        <div id="mos">
+         <h1>Mobile Operting System:</h1> 
+          <ul>
+            {
+              os.map((e)=>{
+                return <li>{e}</li>
+              })
+            }
+          </ul>
+        </div>
+        <div id="mos2">
+          <h1>Mobile Manufacturers:</h1>
+            <ul id="sq">
+              <li>Samsung</li>
+              <li>HTC</li>
+            </ul>
+            <ul>
+              <li>Microsoft
+                <ul>
+                  <li>Apple</li>
+                </ul>
+              </li>
+            </ul>
+        </div>
     </div>
   );
 }
